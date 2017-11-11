44c714c0d33e298f6eb3f3930cb843fdea0028b9
matthieu <matthieu@treussart.com>
2017-11-11 08:23:45 +0100

    Fix bug transaction in admin source

--- admin.py
+++ admin.py
@@ -313,7 +313,7 @@ class SourceSuricataAdmin(admin.ModelAdmin):
                                 except Exception as e:
                                     logger.error(e.__str__())
                 upload_url_http.delay(obj.uri, rulesets_id)
-                messages.add_message(request, messages.SUCCESS, "Upload source in progress. <a href='/admin/home/job/'> View Job</a>", extra_tags='safe')
+                messages.add_message(request, messages.SUCCESS, "Upload source in progress. View Jobs", extra_tags='safe')
             # Upload file
             elif obj.method.name == "Upload file":
                 update_progress(0)

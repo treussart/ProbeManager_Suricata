833e4d58170fa441c199317d395c6075da944a1a
matthieu <matthieu@treussart.com>
2017-12-17 10:50:35 +0100

    Change form when add or change

--- forms.py
+++ forms.py
@@ -5,4 +5,4 @@ from suricata.models import Suricata
 class SuricataChangeForm(ModelForm):
     class Meta:
         model = Suricata
-        fields = ('name', 'description', 'secure_deployment', 'scheduled_enabled', 'server', 'rulesets', 'configuration')
+        fields = ('name', 'description', 'secure_deployment', 'server', 'rulesets', 'configuration')

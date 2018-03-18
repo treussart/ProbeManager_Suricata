374e0f74d52598f611856f011b807687b49c0cd8
matthieu <matthieu@treussart.com>
2018-03-18 11:24:46 +0100

    update tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -9,8 +9,8 @@ from suricata.models import Suricata, SourceSuricata
 
 
 class TasksSuricataTest(TestCase):
-    fixtures = ['init', 'crontab', 'test-suricata-signature', 'test-suricata-script', 'test-suricata-ruleset',
-                'test-suricata-source', 'test-suricata-conf', 'test-suricata-probe']
+    fixtures = ['init', 'crontab', 'test-core-secrets', 'test-suricata-signature', 'test-suricata-script', 'test-suricata-ruleset',
+                'test-suricata-source', 'test-suricata-conf', 'test-suricata-suricata']
 
     @classmethod
     def setUpTestData(cls):

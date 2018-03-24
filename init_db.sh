15c2ce435daf7d6f844f0b7ed822c408cb1de1e1
matthieu <matthieu@treussart.com>
2018-03-24 16:37:54 +0100

    test install prod in travis

--- init_db.sh
+++ init_db.sh
@@ -5,9 +5,6 @@ echo '## Load data Suricata ##'
 if [ -z $1 ] || [[ "$1" = 'dev' ]]; then
     arg="dev"
     dest=""
-elif [[ "$1" = 'travis' ]]; then
-    arg=$1
-    dest=""
 elif [[ "$1" = 'prod' ]]; then
     arg=$1
     if [ -z $2 ]; then

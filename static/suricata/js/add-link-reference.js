0777ef032d4bb84ec425a242a382aed01320ae40
matthieu <matthieu@treussart.com>
2018-04-04 21:29:20 +0200

    CamelCase in Javascript

--- static/suricata/js/add-link-reference.js
+++ static/suricata/js/add-link-reference.js
@@ -1,9 +1,9 @@
 django.jQuery(document).ready(function(){
-    var value_ref = django.jQuery("#id_reference").val();
-    django.jQuery("#id_reference").after( "&nbsp;&nbsp;&nbsp;<a href='http://" + value_ref + "' target='_blank'>" + value_ref + "</a>" );
+    var valueRef = django.jQuery("#id_reference").val();
+    django.jQuery("#id_reference").after( "&nbsp;&nbsp;&nbsp;<a href='http://" + valueRef + "' target='_blank'>" + valueRef + "</a>" );
 });
 
 django.jQuery(document).ready(function(){
-    var value_ref = django.jQuery("#id_uri").val();
-    django.jQuery("#id_uri").after( "&nbsp;&nbsp;&nbsp;<a href='http://" + value_ref + "' target='_blank'>" + value_ref + "</a>" );
+    var valueRef = django.jQuery("#id_uri").val();
+    django.jQuery("#id_uri").after( "&nbsp;&nbsp;&nbsp;<a href='http://" + valueRef + "' target='_blank'>" + valueRef + "</a>" );
 });
--- static/suricata/js/progress-bar.js
+++ static/suricata/js/progress-bar.js
@@ -2,10 +2,10 @@
 django.jQuery(document).ready(function(){
     var width = 0;
     django.jQuery(".submit-row").before("<div id='myProgress'><div id='myBar'>0%</div></div>");
-    var url_protocol = window.location.protocol;
-    var url_hostname = window.location.hostname;
-    var url_port = window.location.port;
-    var url = url_protocol + "//" + url_hostname + ":" + url_port;
+    var urlProtocol = window.location.protocol;
+    var urlHostname = window.location.hostname;
+    var urlPort = window.location.port;
+    var url = urlProtocol + "//" + urlHostname + ":" + urlPort;
 
     setInterval(function(){
             django.jQuery.getJSON(url + "/suricata/get-progress/", function( json ) {

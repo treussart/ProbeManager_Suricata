7fd3b7721f837ab0aa11e829334633ebafd09fe2
matthieu <matthieu@treussart.com>
2018-04-04 21:24:58 +0200

    Fix ' by " in Javascript

--- static/suricata/js/method-options.js
+++ static/suricata/js/method-options.js
@@ -1,57 +1,57 @@
 django.jQuery(document).ready(function(){
-    django.jQuery('#id_scheduled_rules_deployment_enabled').change(function(){
-        if(django.jQuery('#id_scheduled_rules_deployment_enabled').is(':checked')){
-            django.jQuery('.field-scheduled_rules_deployment_crontab').fadeIn("slow");
-            django.jQuery('.field-scheduled_deploy').fadeIn("slow");
+    django.jQuery("#id_scheduled_rules_deployment_enabled").change(function(){
+        if(django.jQuery("#id_scheduled_rules_deployment_enabled").is(":checked")){
+            django.jQuery(".field-scheduled_rules_deployment_crontab").fadeIn("slow");
+            django.jQuery(".field-scheduled_deploy").fadeIn("slow");
         }else{
-            django.jQuery('.field-scheduled_rules_deployment_crontab').fadeOut("fast");
-            django.jQuery('.field-scheduled_deploy').fadeOut("fast");
-            django.jQuery('#id_scheduled_deploy').prop('checked', false);
+            django.jQuery(".field-scheduled_rules_deployment_crontab").fadeOut("fast");
+            django.jQuery(".field-scheduled_deploy").fadeOut("fast");
+            django.jQuery("#id_scheduled_deploy").prop("checked", false);
         }
     });
 
-    if(django.jQuery( "#id_method option:selected" ).text() == 'URL HTTP'){
-        django.jQuery('.field-uri').fadeIn("slow");
-        django.jQuery('.field-scheduled_rules_deployment_enabled').fadeIn("slow");
-        django.jQuery('.field-file').fadeOut("fast");
-    }else if(django.jQuery( "#id_method option:selected" ).text() == 'Upload file'){
-        django.jQuery('.field-file').fadeIn("slow");
-        django.jQuery('.field-uri').fadeOut("fast");
-        django.jQuery('.field-scheduled_rules_deployment_enabled').fadeOut("fast");
-        django.jQuery('.field-scheduled_rules_deployment_crontab').fadeOut("fast");
-        django.jQuery('.field-scheduled_deploy').fadeOut("fast");
-        django.jQuery('#id_scheduled_deploy').prop('checked', false);
-        django.jQuery('#id_scheduled_rules_deployment_enabled').prop('checked', false);
+    if(django.jQuery( "#id_method option:selected" ).text() == "URL HTTP"){
+        django.jQuery(".field-uri").fadeIn("slow");
+        django.jQuery(".field-scheduled_rules_deployment_enabled").fadeIn("slow");
+        django.jQuery(".field-file").fadeOut("fast");
+    }else if(django.jQuery( "#id_method option:selected" ).text() == "Upload file"){
+        django.jQuery(".field-file").fadeIn("slow");
+        django.jQuery(".field-uri").fadeOut("fast");
+        django.jQuery(".field-scheduled_rules_deployment_enabled").fadeOut("fast");
+        django.jQuery(".field-scheduled_rules_deployment_crontab").fadeOut("fast");
+        django.jQuery(".field-scheduled_deploy").fadeOut("fast");
+        django.jQuery("#id_scheduled_deploy").prop("checked", false);
+        django.jQuery("#id_scheduled_rules_deployment_enabled").prop("checked", false);
     }else{
-        django.jQuery('.field-file').fadeOut("fast");
-        django.jQuery('.field-uri').fadeOut("fast");
-        django.jQuery('.field-scheduled_rules_deployment_enabled').fadeOut("fast");
-        django.jQuery('.field-scheduled_rules_deployment_crontab').fadeOut("fast");
-        django.jQuery('.field-scheduled_deploy').fadeOut("fast");
-        django.jQuery('#id_scheduled_deploy').prop('checked', false);
-        django.jQuery('#id_scheduled_rules_deployment_enabled').prop('checked', false);
+        django.jQuery(".field-file").fadeOut("fast");
+        django.jQuery(".field-uri").fadeOut("fast");
+        django.jQuery(".field-scheduled_rules_deployment_enabled").fadeOut("fast");
+        django.jQuery(".field-scheduled_rules_deployment_crontab").fadeOut("fast");
+        django.jQuery(".field-scheduled_deploy").fadeOut("fast");
+        django.jQuery("#id_scheduled_deploy").prop("checked", false);
+        django.jQuery("#id_scheduled_rules_deployment_enabled").prop("checked", false);
     }
-    django.jQuery('#id_method').change(function(){
-        if(django.jQuery( "#id_method option:selected" ).text() == 'URL HTTP'){
-            django.jQuery('.field-uri').fadeIn("slow");
-            django.jQuery('.field-scheduled_rules_deployment_enabled').fadeIn("slow");
-            django.jQuery('.field-file').fadeOut("fast");
-        }else if(django.jQuery( "#id_method option:selected" ).text() == 'Upload file'){
-            django.jQuery('.field-file').fadeIn("slow");
-            django.jQuery('.field-uri').fadeOut("fast");
-            django.jQuery('.field-scheduled_rules_deployment_enabled').fadeOut("fast");
-            django.jQuery('.field-scheduled_rules_deployment_crontab').fadeOut("fast");
-            django.jQuery('.field-scheduled_deploy').fadeOut("fast");
-            django.jQuery('#id_scheduled_deploy').prop('checked', false);
-            django.jQuery('#id_scheduled_rules_deployment_enabled').prop('checked', false);
+    django.jQuery("#id_method").change(function(){
+        if(django.jQuery( "#id_method option:selected" ).text() == "URL HTTP"){
+            django.jQuery(".field-uri").fadeIn("slow");
+            django.jQuery(".field-scheduled_rules_deployment_enabled").fadeIn("slow");
+            django.jQuery(".field-file").fadeOut("fast");
+        }else if(django.jQuery( "#id_method option:selected" ).text() == "Upload file"){
+            django.jQuery(".field-file").fadeIn("slow");
+            django.jQuery(".field-uri").fadeOut("fast");
+            django.jQuery(".field-scheduled_rules_deployment_enabled").fadeOut("fast");
+            django.jQuery(".field-scheduled_rules_deployment_crontab").fadeOut("fast");
+            django.jQuery(".field-scheduled_deploy").fadeOut("fast");
+            django.jQuery("#id_scheduled_deploy").prop("checked", false);
+            django.jQuery("#id_scheduled_rules_deployment_enabled").prop("checked", false);
         }else{
-            django.jQuery('.field-file').fadeOut("fast");
-            django.jQuery('.field-uri').fadeOut("fast");
-            django.jQuery('.field-scheduled_rules_deployment_enabled').fadeOut("fast");
-            django.jQuery('.field-scheduled_rules_deployment_crontab').fadeOut("fast");
-            django.jQuery('.field-scheduled_deploy').fadeOut("fast");
-            django.jQuery('#id_scheduled_deploy').prop('checked', false);
-            django.jQuery('#id_scheduled_rules_deployment_enabled').prop('checked', false);
+            django.jQuery(".field-file").fadeOut("fast");
+            django.jQuery(".field-uri").fadeOut("fast");
+            django.jQuery(".field-scheduled_rules_deployment_enabled").fadeOut("fast");
+            django.jQuery(".field-scheduled_rules_deployment_crontab").fadeOut("fast");
+            django.jQuery(".field-scheduled_deploy").fadeOut("fast");
+            django.jQuery("#id_scheduled_deploy").prop("checked", false);
+            django.jQuery("#id_scheduled_rules_deployment_enabled").prop("checked", false);
         }
     });
 });
--- static/suricata/js/progress-bar.js
+++ static/suricata/js/progress-bar.js
@@ -1,26 +1,26 @@
 
 django.jQuery(document).ready(function(){
     var width = 0;
-    django.jQuery('.submit-row').before('<div id="myProgress"><div id="myBar">0%</div></div>');
+    django.jQuery(".submit-row").before("<div id='myProgress'><div id='myBar'>0%</div></div>");
     var url_protocol = window.location.protocol;
     var url_hostname = window.location.hostname;
-    var url_port = window.location.port
-    var url = url_protocol + '//' + url_hostname + ':' + url_port;
+    var url_port = window.location.port;
+    var url = url_protocol + "//" + url_hostname + ":" + url_port;
 
     setInterval(function(){
             django.jQuery.getJSON(url + "/suricata/get-progress/", function( json ) {
                 if(json){
                     var width = json.progress;
                     var elem = document.getElementById("myBar");
-                    if (typeof(elem) != 'undefined' && elem != null) {
+                    if (typeof(elem) != "undefined" && elem != null) {
                         //console.log(width);
                         if (width <= 100) {
-                            elem.style.width = width + '%';
-                            elem.innerHTML = width + '%';
+                            elem.style.width = width + "%";
+                            elem.innerHTML = width + "%";
                         }
                     }
                 }else{
-                    console.log("getJson not succeed !")
+                    console.log("getJson not succeed !");
                 }
             });
         }, 3000);

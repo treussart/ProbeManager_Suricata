a1966a109aaf3175727920617a2cee3368474cf2
matthieu <matthieu@treussart.com>
2018-04-23 21:54:45 +0200

    Change install script

--- install.sh
+++ install.sh
@@ -17,7 +17,6 @@ if [[ $OSTYPE = *"darwin"* ]]; then
         if ! brew list | grep -qw suricata ; then
             brew install suricata
         fi
-        which suricata
         config="/usr/local/etc/suricata/suricata.yaml"
         rules="/usr/local/etc/suricata/rules"
     fi
@@ -31,7 +30,6 @@ elif [ -f /etc/debian_version ]; then
         tar -xzf suricata-"$SURICATA_VERSION".tar.gz
         (cd suricata-"$SURICATA_VERSION" && ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var && make && sudo make install && sudo make install-conf)
     fi
-    which suricata
     config="/etc/suricata/suricata.yaml"
     rules="/etc/suricata/rules"
     if [[ "$arg" = 'prod' ]]; then
@@ -47,8 +45,6 @@ elif [ -f /etc/debian_version ]; then
         if [ -f /etc/suricata/suricata-debian.yaml ]; then
             mv /etc/suricata/suricata-debian.yaml /etc/suricata/suricata.yaml
         fi
-        which suricata
-        suricata -V
     fi
 fi
 if ! type suricata ; then
@@ -58,3 +54,6 @@ echo "SURICATA_BINARY = '$( which suricata )'" > "$destfull"probemanager/suricat
 echo "SURICATA_CONFIG = '$config'" >> "$destfull"probemanager/suricata/settings.py
 echo "SURICATA_RULES = '$rules'" >> "$destfull"probemanager/suricata/settings.py
 echo "SURICATA_VERSION = '$SURICATA_VERSION'" >> "$destfull"probemanager/suricata/settings.py
+which suricata
+suricata -V
+exit 0

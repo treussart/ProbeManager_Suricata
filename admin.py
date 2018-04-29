1960ba28fc308f2e4214b40c170b8b2b0cd067d7
matthieu <matthieu@treussart.com>
2018-04-29 10:45:20 +0200

    Fix delete check_task

--- admin.py
+++ admin.py
@@ -81,6 +81,12 @@ class SuricataAdmin(admin.ModelAdmin):
             logger.debug(str(periodic_task) + " deleted")
         except PeriodicTask.DoesNotExist:  # pragma: no cover
             pass
+        try:
+            periodic_task = PeriodicTask.objects.get(name=probe.name + "_check_task")
+            periodic_task.delete()
+            logger.debug(str(periodic_task) + " deleted")
+        except PeriodicTask.DoesNotExist:  # pragma: no cover
+            pass
         messages.add_message(request, messages.SUCCESS, "Suricata instance " + probe.name + " deleted")
         super().delete_model(request, obj)
 
